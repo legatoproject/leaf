079f398d7d01e63f8cb776ebc088d312a4214088
tnguyenxuan <nxtung@tma.com.vn>
2020-05-06 11:09:12 +0700

    Change the overridden order for env variables
    The user-defined env variables (user/workspace/profile env) will
    override the env variables that come from packages
    
    Resolves: LETOOLS-454
    Change-Id: I00c6216b04eadf28791854ae28bf2763810b83b4

--- src/leaf/api/workspace.py
+++ src/leaf/api/workspace.py
@@ -296,11 +296,11 @@ class WorkspaceManager(PackageManager):
 
     def build_full_environment(self, profile: Profile):
         self.is_profile_sync(profile, raise_if_not_sync=True)
-        out = self.build_pf_environment(profile)
         ipmap = self.list_installed_packages()
         if not LeafSettings.PROFILE_NORELATIVE.as_boolean():
             ipmap.update(self.list_installed_packages(alt_user_root_folder=profile.folder))
-        out.append(self.build_packages_environment(self.get_profile_dependencies(profile, ipmap=ipmap), ipmap=ipmap))
+        out = self.build_packages_environment(self.get_profile_dependencies(profile, ipmap=ipmap), ipmap=ipmap)
+        out.append(self.build_pf_environment(profile))
         return out
 
     def build_pf_environment(self, profile: Profile):
